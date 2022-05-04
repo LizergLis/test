ed06af1cc01c3649e39febb07c567c8d53e94385
LizergLis <104915821+LizergLis@users.noreply.github.com>
2022-05-04 17:53:03 +0300

    Add files via upload

---
+++ test/test/test/MyButtons.swift
@@ -0,0 +1,22 @@
+//
+//  MyButtons.swift
+//  test
+//
+//  Created by Student on 04.05.2022.
+//
+
+import UIKit
+
+class MyButtons: UIButton {
+    button.layer.cornerRadius = 5
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ test/test/test/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Student on 04.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button.layer.cornerRadius = 10
+        
+    }
+
+   
+    
+}
+
